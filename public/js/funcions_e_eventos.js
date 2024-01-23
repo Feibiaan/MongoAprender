c154f2bd7ca67bffc7d932089ee8472e19d09bf9
afd 23-24 tardes <afdt@alupostal4.local>
2024-01-23 19:23:55 +0100

    segundo commit fabian, añadidos bucles

--- public/js/funcions_e_eventos.js
+++ public/js/funcions_e_eventos.js
@@ -4,11 +4,30 @@ function preparandoDatos(datos) {
         let _div = document.createElement("div");
         document.querySelector(".lista").append(_div);
         let _span = document.createElement("span");
+        
+        // for (let elementos in documento) {
+            //     _span.innerText += documento[elementos] + " ";            
+            // }
+        let tamanhio = Object.keys(documento).length;
+        let elementos = Object.values(documento)
+        
+            for(let contador = 0; contador < tamanhio ;contador ++){   
+                
+                let _span2 = document.createElement("span");
 
-        for (let elementos in documento) {
-            _span.innerText += documento[elementos] + " ";            
+            if(Array.isArray(elementos[contador])){
+                // bucle sobre o array
+                for(let contador2 = 0; contador2 < elementos[contador].length ;contador2 ++){
+                    console.log('dentro: ',_span)
+                    _span2.innerHTML += elementos[contador][contador2];
+                }
+            }
+            _span2.innerHTML += elementos[contador];
+            console.log('dentro fora: ',_span2)
+            _div.append(_span2)
+            
         }
-        _div.append(_span);
+        
     })
 }
 
